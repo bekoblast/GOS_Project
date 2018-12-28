f23afc880f7514795c7d954e16377d3569a86999
BekoBlast <beko1986@gmail.com>
2018-12-28 20:49:00 +0200

    Add project files.

---
+++ G_Inventory/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(G_Inventory.Startup))]
+namespace G_Inventory
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
