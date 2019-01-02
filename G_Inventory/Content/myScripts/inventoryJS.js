23553560beaf05e3c547bfa1d130f562c2669c5e
BekoBlast <beko1986@gmail.com>
2019-01-02 03:33:18 +0200

    stage 2

--- G_Inventory/Content/myScripts/inventoryJS.js
+++ G_Inventory/Content/myScripts/inventoryJS.js
@@ -1,3 +1,95 @@
 ﻿$(document).ready(function () {
-    $('#productsTable').DataTable();
+    $('#productsTable').DataTable({
+        "columnDefs": [
+            {
+                "targets": [0],
+                "visible": false,
+                "searchable": false
+            },
+        ],
+        "pageLength": 100
+    });
+    //==================================================
+    validate();
 });
+//======================================================
+function validate() {
+    $('#addProductForm').validate({
+        rules: {
+            pName: {
+                required: true,
+                minlength: 2
+            },
+            pQuantity: {
+                required: true,
+                digits: true,
+                min: 1
+            },
+            pCost: {
+                required: true,
+                number: true,
+                min: 0.01
+            },
+            pPrice: {
+                required: true,
+                number: true,
+                min: 0.01
+            },
+        },
+        messages: {
+            pName: {
+                required: "Product Name is Required!",
+                minlength: "Product name must consist at least 2 characters"
+            },
+            pQuantity: {
+                required: "Quantity is Required",
+                digits: "Numbers only",
+                min: "Minimum Quantity is 1"
+            },
+            pCost: {
+                required: "Cost is Required",
+                number: "Numbers Only",
+                min: "minimum cost value is 0.01"
+            },
+            pPrice: {
+                required: "Price is Required",
+                number: "Numbers Only",
+                min: "minimum price value is 0.01"
+            },
+        },
+
+    });
+}
+//======================================================
+$(document).on('click', '#submitBtn', function () {
+    if ($('#addProductForm').valid()) {
+        //alert('success');
+        var product = {
+            Name: $('#productName').val(),
+            CategoryID: $('#productCategory').val(),
+            Qty: $('#productQuantity').val(),
+            Cost: $('#productCost').val(),
+            Price: $('#productPrice').val(),
+        }
+
+        $.ajax({
+            url: '/Inventory/AddProduct',
+            method: 'POST',
+            data: product,
+            success: function () {
+                alert('Product added succefully!')
+                $('#AddProductModal').modal('toggle');
+                var table = $('#addProductForm').DataTable({
+                    ajax: "data.json"
+                });
+                table.ajax.reload();
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                alert(jqXHR.responseText);
+
+            }
+        });
+    }
+
+});
+//======================================================
--- G_Inventory/Controllers/InventoryController.cs
+++ G_Inventory/Controllers/InventoryController.cs
@@ -1,16 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
+using System.Text;
 using System.Web.Mvc;
+using G_Inventory.Models;
 
 namespace G_Inventory.Controllers
 {
     public class InventoryController : Controller
     {
+        private GOSEntities db = new GOSEntities();
         public ActionResult Index()
         {
+            var products = db.Products.Include("Category");
+            var categories = db.Categories;
+            ViewBag.productList  = products;
+            ViewBag.categoryList = categories;
             return View();
         }
+        //==============================================================
+        public ActionResult Fetch()
+        {
+            
+            var products = db.Products.Include("Category");
+            StringBuilder html = new StringBuilder();
+            return Content(html.ToString());
+        }
+        //==============================================================
+        [HttpPost]
+        public void AddProduct(Product product)
+        {
+            db.Products.Add(product);
+            db.SaveChanges();
+            //return View();
+        }
+        //==============================================================
     }
 }
